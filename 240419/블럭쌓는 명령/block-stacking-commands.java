a4c49070a8d1100a35d3ce31175321476edc5fca
So_ob <dhrtnqls0535@naver.com>
2024-04-19 23:43:16 +0900

    Title: 블럭쌓는 명령, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240419/블럭쌓는 명령/block-stacking-commands.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int [] arr = new int[N];
+
+        for(int i = 0; i < K; i++){
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            for(int j = start; j <= end; j++){
+                arr[j]++;
+            }
+        }
+
+        List<Integer> list = Arrays.stream(arr).boxed().collect(Collectors.toList());
+        Collections.sort(list);
+
+        System.out.println(list.get(N/2));
+
+    }
+}
