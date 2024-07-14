995d9187f1f5c02ca029a7cf050d27a0777cb6e0
So_ob <dhrtnqls0535@naver.com>
2024-07-14 17:45:26 +0900

    Title: 연쇄로 터지는 폭탄, Time: 105ms, Memory: 8MB, Status: Passed - CodeTree

--- 240714/연쇄로 터지는 폭탄/series-of-bombs-detonating.java
+++ 240714/연쇄로 터지는 폭탄/series-of-bombs-detonating.java
@@ -25,6 +25,7 @@ public class Main {
                     cnt++;
                 }
             }
+
             answer = Math.max(answer, cnt);
         }
         System.out.println(answer);
@@ -33,10 +34,12 @@ public class Main {
     static void boom(int x, int idx, int range){
         visited[idx] = true;
 
+        int max = -1;
+        int min = Integer.MAX_VALUE;
         for(int i = idx - 1; i >= 0 ; i--){
             if(list.get(i) >= x - range && !visited[i]){
                 visited[i] = true;
-                boom(list.get(i), i, range + 1);
+                max = i;
             }
             else
                 break;
@@ -44,10 +47,15 @@ public class Main {
         for(int i = idx + 1; i < list.size() ; i++){
             if(list.get(i) <= x + range && !visited[i]){
                 visited[i] = true;
-                boom(list.get(i), i, range + 1);
+                min = i;
             }
             else
                 break;
         }
+
+        if(min != Integer.MAX_VALUE)
+            boom(list.get(min), min, range + 1);
+        if(max != -1)
+            boom(list.get(max), max, range + 1);
     }
 }
