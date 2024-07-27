2c54a18f5e3ec98b780311de919f42a5f455e9db
So_ob <dhrtnqls0535@naver.com>
2024-07-27 19:01:30 +0900

    Title: 블럭쌓는 명령, Time: 413ms, Memory: 25MB, Status: Passed - CodeTree

--- 240727/블럭쌓는 명령/block-stacking-commands.java
+++ 240727/블럭쌓는 명령/block-stacking-commands.java
@@ -7,18 +7,25 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N+1];
+        int[] arr = new int[N];
 
         for(int i = 0; i < K; i++){
             st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            for(int j = s; j <= e; j++){
-                arr[j]++;
-            }
+            int s = Integer.parseInt(st.nextToken())-1;
+            int e = Integer.parseInt(st.nextToken())-1;
+            arr[s]++;
+            if(e != N-1)
+                arr[e+1]--;
         }
-        
+
+        int sum = 0;
+        for(int i = 0; i < N; i++){
+            arr[i] += sum;
+            sum = arr[i];
+        }
+
         Arrays.sort(arr);
-        System.out.println(arr[N/2+1]);
+
+        System.out.println(arr[N/2]);
     }
 }
