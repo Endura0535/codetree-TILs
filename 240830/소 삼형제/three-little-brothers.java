ac86c0c8cd10e76e394f783e76f38cf182323afe
So_ob <dhrtnqls0535@naver.com>
2024-08-30 22:32:19 +0900

    Title: 소 삼형제, Time: 110ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240830/소 삼형제/three-little-brothers.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        TreeMap<String, Integer> map = new TreeMap<>();
+    
+        for(int i = 0; i < n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String word1 = st.nextToken();
+            String word2 = st.nextToken();
+            String word3 = st.nextToken();
+            List<String> list = new ArrayList<>();
+            list.add(word1);
+            list.add(word2);
+            list.add(word3);
+            Collections.sort(list);
+            String totalWord = "";
+            for(String str : list){
+                totalWord += str + " ";
+            }
+            if(map.containsKey(totalWord))
+                map.put(totalWord, map.get(totalWord)+1);
+            else
+                map.put(totalWord, 1);
+        }
+
+        System.out.println(map.firstEntry().getValue());
+
+
+
+
+    }
+}
