8fffbff09cf0ac37589cc5b9b02c5b30a32d65e3
So_ob <dhrtnqls0535@naver.com>
2024-10-30 23:04:28 +0900

    Title: 거울에 레이저 쏘기, Time: 503ms, Memory: 51MB, Status: Passed - CodeTree

--- 241030/거울에 레이저 쏘기/shoot-a-laser-in-the-mirror.java
+++ 241030/거울에 레이저 쏘기/shoot-a-laser-in-the-mirror.java
@@ -1,26 +1,23 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
-    // 우상좌하
-    static int[] dx = {1, 0, -1, 0};
+public class Main{
+    static int[] dx = {1, 0, -1, 0};    // 우상좌하
     static int[] dy = {0, -1, 0, 1};
-
-    static int[][] map;
-    static int answer = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        map = new int[N + 2][M + 2];
+        int[][] map = new int[N + 2][M + 2];
         for (int i = 0; i < N + 2; i++) {
             Arrays.fill(map[i], -1);
         }
         for (int i = 0; i < N; i++) {
             String str = br.readLine();
             for (int j = 0; j < M; j++) {
+                map[i + 1][j + 1] = str.charAt(j) == '/' ? 1 : 0;
                 if (str.charAt(j) == '/')
                     map[i + 1][j + 1] = 1;
                 else
@@ -28,31 +25,47 @@ public class Main {
             }
         }
 
+        Queue<Razor> q = new LinkedList<>();
         for (int i = 1; i <= N; i++) {
-            DFS(1, i, 0, 0);
-            DFS(M, i, 2, 0);
+            q.add(new Razor(1, i, 0));
+            q.add(new Razor(M, i, 2));
         }
         for (int i = 1; i <= M; i++) {
-            DFS(i, 1, 3, 0);
-            DFS(i, N, 1, 0);
+            q.add(new Razor(i, 1, 3));
+            q.add(new Razor(i, N, 1));
         }
 
-        System.out.println(answer);
+        int cnt = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int s = 0; s < size; s++) {
+                Razor r = q.poll();
+                int x = r.x;
+                int y = r.y;
+                int d = r.d;
+                if (map[y][x] == -1)
+                    continue;
+
+                if (map[y][x] == 0) {   // / : 우상좌하 -> 하좌상우
+                    q.add(new Razor(x + dx[3 - d], y + dy[3 - d], 3 - d));
+                } else {     // \ : 우상좌하 -> 상우하좌
+                    int nd = d % 2 == 0 ? d + 1 : d - 1;
+                    q.add(new Razor(x + dx[nd], y + dy[nd], nd));
+                }
+            }
+            cnt++;
+        }
+
+        System.out.println(cnt - 1);
     }
 
-    public static void DFS(int x, int y, int d, int cnt) {
-        if (cnt != 0) {
-            if (map[y][x] == -1) {
-                answer = Math.max(answer, cnt);
-                return;
-            }
-        }
+    public static class Razor {
+        int x, y, d;
 
-        if (map[y][x] == 0) {   // / : 우상좌하 -> 하좌상우
-            DFS(x + dx[3 - d], y + dy[3 - d], 3 - d, cnt + 1);
-        } else {     // \ : 우상좌하 -> 상우하좌
-            int nd = d % 2 == 0 ? d + 1 : d - 1;
-            DFS(x + dx[nd], y + dy[nd], nd, cnt + 1);
+        public Razor(int x, int y, int d) {
+            this.x = x;
+            this.y = y;
+            this.d = d;
         }
     }
 }
