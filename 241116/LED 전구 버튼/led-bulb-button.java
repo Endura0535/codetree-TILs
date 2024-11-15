3d166179cc6604ebf278cd1e07cbe6127a561fc8
So_ob <dhrtnqls0535@naver.com>
2024-11-16 04:48:05 +0900

    Title: LED 전구 버튼, Time: 3031ms, Memory: 122MB, Status: Time Limit Exceed - CodeTree

--- 241116/LED 전구 버튼/led-bulb-button.java
+++ 241116/LED 전구 버튼/led-bulb-button.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
@@ -8,44 +9,40 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
+        BigInteger B = BigInteger.valueOf(Long.parseLong(st.nextToken()));
         List<Integer> onList = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             int x = Integer.parseInt(br.readLine());
-            if (x == 1)
+            if (x == 1) {
                 onList.add(i);
+            }
         }
 
-        int cycle = 0;
-        List<List<Integer>> cycleList = new ArrayList<>();
-        int idx = B;
-        while (idx > 0) {
-            cycleList.add(new ArrayList<>(onList));
-            if(cycleList.equals(onList)){
-                cycle = B - idx;
+        BigInteger idx = B;
+
+        while (idx.compareTo(BigInteger.ZERO) > 0) {
+            if(onList.isEmpty())
                 break;
-            }
-            int size = onList.size();
+
             List<Integer> temp = new ArrayList<>(onList);
             for (int x : onList) {
                 int next = (x + 1) % N;
-                if (onList.contains(next))
-                    temp.remove(temp.indexOf(next));
-                else
+                if (onList.contains(next)) {
+                    temp.remove(Integer.valueOf(next));
+                } else {
                     temp.add(next);
+                }
             }
             onList = temp;
-            idx--;
+            idx = idx.subtract(BigInteger.ONE);
         }
 
-        if(cycle != 0)
-            onList = cycleList.get(B % cycle);
-
         for (int i = 0; i < N; i++) {
-            if (onList.contains(i))
+            if (onList.contains(i)) {
                 System.out.println(1);
-            else
+            } else {
                 System.out.println(0);
+            }
         }
     }
 }
