c9c5285f170b90042c8fb73f9fbf7c6f8aa97f0c
So_ob <dhrtnqls0535@naver.com>
2024-12-21 19:40:55 +0900

    Title: 번호순으로 사진찍기, Time: 238ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241221/번호순으로 사진찍기/take-pictures-in-numerical-order.java
@@ -0,0 +1,46 @@
+import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        List<Point> list = new ArrayList<>();
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            list.add(new Point(Math.min(a, b), Math.max(a, b)));
+        }
+        Collections.sort(list, (o1, o2) -> {
+            if (o1.y == o2.y)
+                return o1.x - o2.x;
+            return o1.y - o2.y;
+        });
+
+        int answer = 0;
+        int idx = 0;
+        for (int i = 0; i < list.size() - 1; i++) {
+            Point now = list.get(i);
+            if (now.x < idx) {
+                idx = now.y - 1;
+                continue;
+            }
+            answer++;
+            idx = now.y - 1;
+        }
+
+        if (idx < N)
+            answer++;
+
+        System.out.println(answer);
+    }
+}
