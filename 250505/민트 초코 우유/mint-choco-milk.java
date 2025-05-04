1eb8989d436b448ef45c9083ed836fd8d7712e38
So_ob <dhrtnqls0535@naver.com>
2025-05-05 01:28:51 +0900

    Title: 민트 초코 우유, Time: 282ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250505/민트 초코 우유/mint-choco-milk.java
+++ 250505/민트 초코 우유/mint-choco-milk.java
@@ -5,98 +5,89 @@ public class Main {
     static final int[] dx = {-1, 1, 0, 0};
     static final int[] dy = {0, 0, -1, 1};
 
-    static int N, T;
+    static int n, t;
     static int[][] food = new int[MAX][MAX];
     static int[][] faith = new int[MAX][MAX];
     static boolean[][] visited = new boolean[MAX][MAX];
     static boolean[][] defended = new boolean[MAX][MAX];
-    static String[] input = new String[MAX];
-
     static List<Leader> leaders = new ArrayList<>();
     static List<Member> group = new ArrayList<>();
 
-    static class Pos {
+    static class Pair {
         int x, y;
-        Pos(int x, int y) {
+        Pair(int x, int y) {
             this.x = x;
             this.y = y;
         }
     }
 
     static class Leader {
-        int foodCount, negFaith;
-        Pos pos;
-        Leader(int foodCount, int negFaith, int x, int y) {
-            this.foodCount = foodCount;
+        int typeCount, negFaith;
+        Pair pos;
+        Leader(int typeCount, int negFaith, Pair pos) {
+            this.typeCount = typeCount;
             this.negFaith = negFaith;
-            this.pos = new Pos(x, y);
+            this.pos = pos;
         }
     }
 
     static class Member {
         int negFaith;
-        Pos pos;
-        Member(int negFaith, int x, int y) {
+        Pair pos;
+        Member(int negFaith, Pair pos) {
             this.negFaith = negFaith;
-            this.pos = new Pos(x, y);
+            this.pos = pos;
         }
     }
 
-    static boolean outOfRange(int x, int y) {
-        return x < 1 || x > N || y < 1 || y > N;
+    static boolean out(int x, int y) {
+        return x < 1 || x > n || y < 1 || y > n;
     }
 
-    static int countBits(int val) {
-        int cnt = 0;
-        for (int b = 1; b <= 4; b <<= 1) {
-            if ((val & b) > 0) cnt++;
+    static int countType(int mask) {
+        int count = 0;
+        for (int i = 0; i < 3; i++) {
+            if ((mask & (1 << i)) != 0) count++;
         }
-        return cnt;
+        return count;
     }
 
-    static void dfs(int x, int y, int target) {
+    static void dfs(int x, int y, int val) {
         visited[x][y] = true;
-        group.add(new Member(-faith[x][y], x, y));
-
+        group.add(new Member(-faith[x][y], new Pair(x, y)));
         for (int d = 0; d < 4; d++) {
             int nx = x + dx[d];
             int ny = y + dy[d];
-            if (outOfRange(nx, ny) || visited[nx][ny]) continue;
-            if (food[nx][ny] != target) continue;
-            dfs(nx, ny, target);
+            if (out(nx, ny) || visited[nx][ny] || food[nx][ny] != val) continue;
+            dfs(nx, ny, val);
         }
     }
 
     static void lunch() {
         leaders.clear();
-        for (int i = 1; i <= N; i++) Arrays.fill(visited[i], false);
-
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
+        for (int i = 1; i <= n; i++) Arrays.fill(visited[i], false);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
                 if (visited[i][j]) continue;
                 group.clear();
-                dfs(i, j, food[i][j]);
-
+                int val = food[i][j];
+                dfs(i, j, val);
                 group.sort((a, b) -> {
                     if (a.negFaith != b.negFaith) return Integer.compare(a.negFaith, b.negFaith);
                     if (a.pos.x != b.pos.x) return Integer.compare(a.pos.x, b.pos.x);
                     return Integer.compare(a.pos.y, b.pos.y);
                 });
-
-                int lx = group.get(0).pos.x;
-                int ly = group.get(0).pos.y;
-                faith[lx][ly] += group.size();
-                int count = countBits(food[i][j]);
-                leaders.add(new Leader(count, -faith[lx][ly], lx, ly));
+                Pair leader = group.get(0).pos;
+                faith[leader.x][leader.y] += group.size();
+                leaders.add(new Leader(countType(val), -faith[leader.x][leader.y], leader));
             }
         }
     }
 
     static void evening() {
-        for (int i = 1; i <= N; i++) Arrays.fill(defended[i], false);
-
+        for (int i = 1; i <= n; i++) Arrays.fill(defended[i], false);
         leaders.sort((a, b) -> {
-            if (a.foodCount != b.foodCount) return Integer.compare(a.foodCount, b.foodCount);
+            if (a.typeCount != b.typeCount) return Integer.compare(a.typeCount, b.typeCount);
             if (a.negFaith != b.negFaith) return Integer.compare(a.negFaith, b.negFaith);
             if (a.pos.x != b.pos.x) return Integer.compare(a.pos.x, b.pos.x);
             return Integer.compare(a.pos.y, b.pos.y);
@@ -105,70 +96,65 @@ public class Main {
         for (Leader l : leaders) {
             int x = l.pos.x, y = l.pos.y;
             if (defended[x][y]) continue;
-
             int dir = faith[x][y] % 4;
-            int pow = faith[x][y] - 1;
+            int power = faith[x][y] - 1;
             faith[x][y] = 1;
             int cur = food[x][y];
-
+            int nx = x, ny = y;
             while (true) {
-                x += dx[dir];
-                y += dy[dir];
-                if (outOfRange(x, y)) break;
-                if (food[x][y] == cur) continue;
+                nx += dx[dir];
+                ny += dy[dir];
+                if (out(nx, ny)) break;
+                if (food[nx][ny] == cur) continue;
 
-                int targetFaith = faith[x][y];
-
-                if (pow > targetFaith) {
-                    faith[x][y]++;
-                    pow -= (targetFaith + 1);
-                    food[x][y] = cur;
-                    defended[x][y] = true;
+                int targetFaith = faith[nx][ny];
+                if (power > targetFaith) {
+                    power -= (targetFaith + 1);
+                    faith[nx][ny] = targetFaith + 1;
+                    food[nx][ny] = cur;
+                    defended[nx][ny] = true;
                 } else {
-                    faith[x][y] += pow;
-                    food[x][y] |= cur;
-                    defended[x][y] = true;
+                    faith[nx][ny] += power;
+                    food[nx][ny] |= cur;
+                    defended[nx][ny] = true;
                     break;
                 }
             }
         }
     }
 
-    static void output() {
-        int[] sum = new int[8];
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                sum[food[i][j]] += faith[i][j];
+    static void printResult() {
+        int[] res = new int[8];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                res[food[i][j]] += faith[i][j];
             }
         }
-        System.out.println(sum[7] + " " + sum[3] + " " + sum[5] + " " + sum[6] + " " + sum[4] + " " + sum[2] + " " + sum[1]);
+        System.out.println(res[7] + " " + res[3] + " " + res[5] + " " + res[6] + " " + res[4] + " " + res[2] + " " + res[1]);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        T = sc.nextInt();
-
-        for (int i = 1; i <= N; i++) {
-            input[i] = sc.next();
-            for (int j = 1; j <= N; j++) {
-                char ch = input[i].charAt(j - 1);
-                if (ch == 'T') food[i][j] = 1;
-                else if (ch == 'C') food[i][j] = 2;
-                else if (ch == 'M') food[i][j] = 4;
+        n = sc.nextInt();
+        t = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            String s = sc.next();
+            for (int j = 1; j <= n; j++) {
+                char c = s.charAt(j - 1);
+                if (c == 'T') food[i][j] = 1;
+                else if (c == 'C') food[i][j] = 2;
+                else food[i][j] = 4;
             }
         }
-
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
                 faith[i][j] = sc.nextInt();
             }
         }
-
-        for (int t = 0; t < T; t++) {
+        while (t-- > 0) {
             lunch();
             evening();
-            output();
+            printResult();
         }
     }
 }
